9545bc7bcc4ddb4e527d2abf210982b335c8cce0
Maopp <1121266440@qq.com>
2018-12-06 14:12:11 +0800

    pro init

---
+++ src/main/java/com/catpp/springcloudpro/SpringcloudproApplication.java
@@ -0,0 +1,12 @@
+package com.catpp.springcloudpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudproApplication.class, args);
+	}
+}
